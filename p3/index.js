8cbf83e5e99269a150c9aae4349994bc0125f862
TWCS911 <ijaaroija@gmail.com>
2024-09-17 15:03:50 +0700

    P3 PAW2

---
+++ p3/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const route = require("./routes/index.js");
+
+app.get('/', (req, res) =>{
+    res.send('AKU IJA');
+});
+
+app.use(route);
+
+// app.get('/mahasiswa', (req, res) => {
+//     res.send("Get Data Mahasiswa")
+// });
+
+// app.post('/mahasiswa', (req, res) => {
+//     res.send("Post Mahasiswa");
+// });
+
+// app.put('/mahasiswa/:npm', (req, res) => {
+//     res.send("Put Mahasiswa "+ req.params.npm);
+// });
+
+// app.delete('/mahasiswa/:npm', (req, res) => {
+//     res.send("Delete Mahasiswa "+req.params.npm);
+// });
+
+app.listen(port, () =>{
+    console.log('Example app listening on port 3000')
+});
+
