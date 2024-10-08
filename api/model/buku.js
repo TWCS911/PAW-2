7db3e30ff355bf548cab0dfc26493b9e35105f51
donnaagn <donnaagnesiachandra@gmail.com>
2024-10-08 15:12:01 +0700

    API

---
+++ api/model/buku.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongose");
+
+const bukuSchema = new mongoose.Schema({
+    judul : {type : String, required : true},
+    penulis : {type : String, required: true},
+    genre : [{type : String, required : true}]
+});
+module.exports = mongoose.model("Buku", bukuSchema);
